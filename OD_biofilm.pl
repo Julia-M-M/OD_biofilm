145dfbb796f284256ae7caf7934c22bd62116e8b
Julia Moreno-Manjón <gulia_94@hotmail.es>
2022-03-23 12:00:01 +0000

    First steps

--- OD_biofilm.pl
+++ OD_biofilm.pl
@@ -1,31 +1,81 @@
 #!/usr/bin/env perl
-use strict;
-use warnings;
-use 5.010;
-use Getopt::Long;
-
-# Select input file
-#my ( $dir, $DEBUG, $filename );
-
-#$dir //= '.';
-my $dir = '/Users/jmm/Desktop/Doctorado/OXFORD/4_Metadata';
-my $filename = '*.txt';
 
 
-#GetOptions(
-#    'd|dir|directory=s'  => \$dir,
-#    'db|debug'   => \$DEBUG
-#);
-#if (!defined $dir){
-#	$dir = '.';
-	
-#}
+#Open file
+my @file = glob("/Users/jmm/Desktop/Doctorado/OXFORD/4_Metadata/BIGSdb_040603_1365734700_35217.txt") or die "ERROR $!";
+say "locus\tallele_id\tsequence\tstatus";
+
+"""
+next(file) #Skip header
+data = file.readlines()
+file.close()
+
+print(data) #The data is: 'id\tOD_neg_control\tOD_neg_control_SD\tOD_sample\n'
 
 
+# Set variables
+column = 0
+id=[]
+for x in data:
+    id.append(x.split()[column])
+#print(id)
 
-# Output a tab-delimited text in format:    ID   biofilm_production
-say "ID\tbiofilm_production";
+column = 1
+OD_neg_control=[]
+for x in data:
+    OD_neg_control.append(x.split()[column])
+#print(OD_neg_control)
 
-return 
+column = 2
+OD_neg_control_SD=[]
+for x in data:
+    OD_neg_control_SD.append(x.split()[column])
+#print(OD_neg_control_SD)
+
+column = 3
+OD_sample=[]
+for x in data:
+    OD_sample.append(x.split()[column])
+#print(OD_sample)
 
 
+"""
+id = 7376
+OD_neg_control = -0.000514644
+OD_neg_control_SD = 0.002276625
+OD_sample = 0.042475897
+"""
+
+# ODc formula
+def ODc_formula(OD_neg_control, OD_neg_control_SD):
+    ODc = OD_neg_control + (3*OD_neg_control_SD)
+    return ODc
+
+ODc = ODc_formula(OD_neg_control, OD_neg_control_SD)
+#print(ODc)
+
+
+# Biofilm production conditions
+def biofilm_production_formula(OD_sample, ODc):
+    if OD_sample <= ODc:
+        return "none"
+
+    elif ODc < OD_sample and OD_sample <= 2*ODc:
+        return "low"
+
+    elif 2*ODc < OD_sample and OD_sample <= 4*ODc:
+        return "medium"
+
+    elif 4*ODc < OD_sample:
+        return "high"
+
+    else:
+        return "null"
+
+biofilm_production = biofilm_production_formula(OD_sample, ODc)
+print(biofilm_production)
+
+
+# Output
+print("id\tbiofilm_production\n", id, biofilm_production)
+"""
