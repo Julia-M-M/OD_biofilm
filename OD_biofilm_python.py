053a2fbf17562ce76c73a5b2d51b74f8a5e6ac9c
Julia Moreno-Manjón <gulia_94@hotmail.es>
2022-03-23 11:46:50 +0000

    Dead end

--- OD_biofilm_python.py
+++ OD_biofilm_python.py
@@ -1,46 +1,52 @@
 #Open file
-file = []
-with open("/Users/jmm/Desktop/Doctorado/OXFORD/4_Metadata/BIGSdb_040603_1365734700_35217.txt") as file:
+file = open("/Users/jmm/Desktop/Doctorado/OXFORD/4_Metadata/BIGSdb_040603_1365734700_35217.txt") 
+next(file) #Skip header
 data = file.readlines()
+file.close()
 
-print(data) 
+#print(data) #The data is: 'id\tOD_neg_control\tOD_neg_control_SD\tOD_sample\n'
 
 
 # Set variables
-"""
 column = 0
 id=[]
 for x in data:
     id.append(x.split()[column])
-print(id)
+#print(id)
 
 column = 1
 OD_neg_control=[]
 for x in data:
     OD_neg_control.append(x.split()[column])
-print(OD_neg_control)
+#print(OD_neg_control)
 
 column = 2
 OD_neg_control_SD=[]
 for x in data:
     OD_neg_control_SD.append(x.split()[column])
-print(OD_neg_control_SD)
-"""
+#print(OD_neg_control_SD)
 
+column = 3
+OD_sample=[]
+for x in data:
+    OD_sample.append(x.split()[column])
+#print(OD_sample)
+
+
+"""
 id = 7376
 OD_neg_control = -0.000514644
 OD_neg_control_SD = 0.002276625
 OD_sample = 0.042475897
-
+"""
 
 # ODc formula
 def ODc_formula(OD_neg_control, OD_neg_control_SD):
     ODc = OD_neg_control + (3*OD_neg_control_SD)
     return ODc
 
-for id in data: 
-    ODc = ODc_formula(OD_neg_control, OD_neg_control_SD)
-    print(ODc)
+ODc = ODc_formula(OD_neg_control, OD_neg_control_SD)
+#print(ODc)
 
 
 # Biofilm production conditions
@@ -60,10 +66,9 @@ def biofilm_production_formula(OD_sample, ODc):
     else:
         return "null"
 
-for id in data: 
-    biofilm_production = biofilm_production_formula(OD_sample, ODc)
-    print(biofilm_production)
+biofilm_production = biofilm_production_formula(OD_sample, ODc)
+print(biofilm_production)
 
 
 # Output
-print("id\tbiofilm_production\n", id, biofilm_production_result)
+print("id\tbiofilm_production\n", id, biofilm_production)
