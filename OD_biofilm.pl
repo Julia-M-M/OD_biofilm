a66f67b32d97897ab8473fb8d8810d5e1e9f70e9
Julia Moreno-Manjón <84106177+Julia-M-M@users.noreply.github.com>
2022-03-29 13:00:15 +0100

    Final version

--- OD_biofilm.pl
+++ OD_biofilm.pl
@@ -1,61 +1,68 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Getopt::Long;
 use 5.010;
-
 
 # Input file
 my ( $dir, $DEBUG, $filename );
 GetOptions(
-    'd|dir|directory=s'  => \$dir,
-    'db|debug'   => \$DEBUG
+	'f|file=s' => \$filename,
+	'db|debug' => \$DEBUG
 );
-if (!defined $dir){
-	$dir = '.';
-	
+if ( !defined $filename ) {
+	say "No filename passed.";
+	exit;
 }
 
+open( my $fh, "<", $filename ) || die "Cannot open $filename";
+say "id\tbiofilm_production";
+while ( my $line = <$fh> ) {
+	next if !$line;    #Ignore empty lines.
+	next
+	  if $line =~ /^\D/
+	  ; #Ignore lines that start with a non numeric character (like the header).
+		# Result
+	my ( $isolate_id, $OD_neg_control, $OD_neg_control_SD, $OD_sample ) =
+	  split /\t/, $line;
+	my $biofilm_production =
+	  biofilm_production( $OD_neg_control, $OD_neg_control_SD, $OD_sample );
 
-open (my $fh, "<", $filename) || die "Cannot open $filename";
-while (my $line = <$fh>){
-	next if !$line; #Ignore empty lines.
-	# Result
-    my ($isolate_id, $OD_neg_control, $OD_neg_control_SD, $OD_sample) = split/\t/,$line;
-	my $biofilm_production = biofilm_production($OD_neg_control, $OD_neg_control_SD, $OD_sample);
-# Output
-say "id\tbiofilm_production\n$isolate_id\t$biofilm_production";
+	# Output
+	say "$isolate_id\t$biofilm_production";
 }
 
-
-
-                            ##SUBROUTINES##
-# ODc formula 
+##SUBROUTINES##
+# ODc formula
 sub ODc_formula {
-    my $ODc = $OD_neg_control + (3 * $OD_neg_control_SD);
-    return $ODc;
+	my ( $OD_neg_control, $OD_neg_control_SD ) =
+	  @_;    #Pass the fields to another place (biofilm_production subroutine)
+	my $ODc = $OD_neg_control + ( 3 * $OD_neg_control_SD );
+	return $ODc;
 }
-
 
 # Biofilm production conditions
 sub biofilm_production {
-    my ($OD_neg_control, $OD_neg_control_SD, $OD_sample) = @_;
-    if ($OD_sample <= $ODc){
-        return "none";
-    }
+	my ( $OD_neg_control, $OD_neg_control_SD, $OD_sample ) =
+	  @_;
+	my $ODc = ODc_formula( $OD_neg_control, $OD_neg_control_SD );
+	if ( $OD_sample <= $ODc ) {
+		return "none";
+	}
 
-    if ($ODc < $OD_sample and $OD_sample <= 2*$ODc){
-        return "low";
-    }
+	if ( $ODc < $OD_sample and $OD_sample <= 2 * $ODc ) {
+		return "low";
+	}
 
-    if (2*$ODc < $OD_sample and $OD_sample <= 4*$ODc){
-        return "medium";
-    }
+	if ( 2 * $ODc < $OD_sample and $OD_sample <= 4 * $ODc ) {
+		return "medium";
+	}
 
-    if (4*$ODc < $OD_sample){
-        return "high";
-    }
+	if ( 4 * $ODc < $OD_sample ) {
+		return "high";
+	}
 
-    else {
-        return "null";
-    }
+	else {
+		return "null";
+	}
 }
